d65eab894649658db10d3f7ddd25c008d868cb54
Darmostuk Aleksandr <sasha0darmostuk@gmail.com>
2025-12-12 23:38:42 +0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import { globalIgnores } from "eslint/config";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      extends: ["airbnb-base/legacy"]
+    },
+  },
+  globalIgnores(['dist/'])
+];
